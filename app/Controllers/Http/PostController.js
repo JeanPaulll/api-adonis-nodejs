d0e854bf64c58d8391858b565748f37df8e34ce2
Jean Paul <jeanpaulwebb@gmail.com>
2019-10-08 17:44:12 -0300

    Routes e PostController CRUD

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -1,93 +1,120 @@
 'use strict'
-
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
+
 /** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const Post = use('App/Models/Post');
+const { validateAll } = use('Validator')
 
 /**
  * Resourceful controller for interacting with posts
  */
 class PostController {
-  /**
-   * Show a list of all posts.
-   * GET posts
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index ({ request, response, view }) {
-  }
+    /**
+     * Show a list of all posts.
+     * GET posts
+     *
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     * @param {View} ctx.view
+     */
+    async index({request, response, view}) {
+       return Post.all();
+    }
 
-  /**
-   * Render a form to be used for creating a new post.
-   * GET posts/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
-  }
+    /**
+     * Render a form to be used for creating a new post.
+     * GET posts/create
+     *
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     * @param {View} ctx.view
+     */
+    async create({request, response, view}) {
+    }
 
-  /**
-   * Create/save a new post.
-   * POST posts
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response }) {
-  }
+    /**
+     * Create/save a new post.
+     * POST posts
+     *
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     */
+    async store({request, response}) {
+        const data = request.only(['titulo', 'descricao']);
+        await Post.create({
+            "titulo" : "Teste Titulo",
+            "descricao" : "Teste Descrição",
+        })
+    }
 
-  /**
-   * Display a single post.
-   * GET posts/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async show ({ params, request, response, view }) {
-  }
+    /**
+     * Display a single post.
+     * GET posts/:id
+     *
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     * @param {View} ctx.view
+     */
+    async show({params, request, response, view}) {
+        const data = await Post.findOrFail(params.id);
+        await data.load('images');
+        return data
+    }
 
-  /**
-   * Render a form to update an existing post.
-   * GET posts/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
+    /**
+     * Render a form to update an existing post.
+     * GET posts/:id/edit
+     *
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     * @param {View} ctx.view
+     */
+    async edit({params, request, response, view}) {
+        const post = await Post.findOrFail(params.id);
+        return { post: post.toJSON() };
+    }
 
-  /**
-   * Update post details.
-   * PUT or PATCH posts/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update ({ params, request, response }) {
-  }
+    /**
+     * Update post details.
+     * PUT or PATCH posts/:id
+     *
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     */
+    async update({params, request, response}) {
+        const data = request.only(['titulo', 'descricao']);
+        const validation = await validateAll(data, {
+            titulo: 'required',
+            descricao: 'required',
+        });
+        const post = await Post.findOrFail(params.id);
+        post.merge(data);
+        await post.save();
+    }
 
-  /**
-   * Delete a post with id.
-   * DELETE posts/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy ({ params, request, response }) {
-  }
+    /**
+     * Delete a post with id.
+     * DELETE posts/:id
+     *
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     */
+    async destroy({params, request, response}) {
+        const data = await Post.findOrFail(params.id);
+        if (data.user_id !== auth.user.id) {
+            return response.status(401).send({ error: 'Não autorizado!' })
+        }
+        await data.delete()
+    }
 }
 
 module.exports = PostController
--- start/routes.js
+++ start/routes.js
@@ -16,4 +16,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+Route.on('/').render('welcome');
+// Route.get('posts', 'PostController.index').as('post.index');
+Route.resource('posts', 'PostController')
+    .apiOnly();
